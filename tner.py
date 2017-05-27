de7e5a33d63fbd104e9eb2f123abce522bfb1db3
Orz <kakinuma4ko@gmail.com>
2017-05-28 04:35:10 +0800

    Update tner.py

--- tner.py
+++ tner.py
@@ -4,12 +4,18 @@ import os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+DATAPATH = './data/data/'
+JSONPATH = './data/json/'
+TEMPPATH = './data/temp/'
+STFPATH = './data/stf/'
+NLTKPATH = './data/nltk/'
+
 files = []
 for file in os.listdir(DATAPATH):
     if file[0] != '.':
         files.append(file)
 
-def myLog(str):
+def Log(str):
     sys.stdout.write('\r')
     sys.stdout.write(str)
     sys.stdout.flush()
