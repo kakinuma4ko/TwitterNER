8635bab2c6843b1bdec873be33e303a966f56208
Orz <kakinuma4ko@gmail.com>
2017-05-27 22:53:26 +0800

    Update predo.py

--- predo.py
+++ predo.py
@@ -16,3 +16,26 @@ for filename in tner.files:
     tempFile.write(']}')
     tempFile.close()
     dataFile.close()
+
+import json
+import re
+
+for filename in tner.files:
+    tempFile = codecs.open(tner.TEMPPATH + filename+'.temp', 'r', 'utf-8')
+    jsonFile = codecs.open(tner.JSONPATH + filename+'.json', 'w', 'utf-8')
+
+    subJson = json.load(tempFile)
+    jsonTextArray = subJson['subtext']
+
+    for jsonText in jsonTextArray:
+        tner.myLog(str(jsonText['num']))
+        httpsPattern = re.compile('https://t.co/\w*', re.S)
+        items = re.findall(httpsPattern, jsonText['text'])
+        httpsArray = []
+        for item in items:
+            jsonText['text'] = jsonText['text'].replace(item, b'')
+            httpsArray.append(item)
+        # print jsonText['text']
+        jsonText['http'] = httpsArray
+
+    json.dump(subJson,jsonFile)
